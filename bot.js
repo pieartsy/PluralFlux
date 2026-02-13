c3cea8e0926a80c012d6a3656740f9834abb5c22
Aster Fialla <asterfialla@gmail.com>
2026-02-13 00:36:56 -0500

    deleting orignal message

--- bot.js
+++ bot.js
@@ -65,8 +65,8 @@ async function echo(api, data, command_name) {
     const message = data.content.slice(prefix.length + command_name.length).trim();
     if (message.length > 0) {
         const webhook = await getOrCreateWebhook(api, data);
-        console.log(data.author);
         await api.webhooks.execute(webhook.id, webhook.token, {content: message, username: data.author.global_name});
+        await api.channels.deleteMessage(data.channel_id, data.id);
     }
     else {
         await api.channels.createMessage(data.channel_id, {content: '(Please input a message!)'});
