e410cd41f3777f0d4ae7d5a47859a3684453c668
Aster Fialla <asterfialla@gmail.com>
2026-02-13 17:07:10 -0500

    passed only channel id into webhook helper methods

--- helpers/webhookHelper.js
+++ helpers/webhookHelper.js
@@ -1,16 +1,16 @@
 const wh = {};
 
-wh.get_or_create_webhook = async function (api, data) {
+wh.get_or_create_webhook = async function (api, channel_id) {
     const name = 'PluralFlux Proxy Webhook';
-    let webhook = await get_webhook(api, data, name);
+    let webhook = await get_webhook(api, channel_id, name);
     if (webhook === undefined) {
-        webhook = await api.channels.createWebhook(data.channel_id, {name: name});
+        webhook = await api.channels.createWebhook(channel_id, {name: name});
     }
     return webhook;
 }
 
-async function get_webhook(api, data, name) {
-    const all_webhooks = await api.channels.getWebhooks(data.channel_id);
+async function get_webhook(api, channel_id, name) {
+    const all_webhooks = await api.channels.getWebhooks(channel_id);
     if (all_webhooks.length === 0) {
         return;
     }
@@ -21,6 +21,17 @@ async function get_webhook(api, data, name) {
         }
     })
     return pf_webhook;
+}
+
+wh.replace_message = async function (api, channel_id, text, member) {
+    if (text.length > 0) {
+        const webhook = await wh.get_or_create_webhook(api, channel_id);
+        await api.webhooks.execute(webhook.id, webhook.token, {content: text, username: member.displayname ?? member.name, propic: member.propic});
+        await api.channels.deleteMessage(channel_id, data.id);
+    }
+    else {
+        await api.channels.createMessage(channel_id, {content: '(Please input a message!)'});
+    }
 }
 
 export const webhookHelper = wh;
