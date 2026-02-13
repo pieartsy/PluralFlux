f75257ee92474f9998b683f4d4e4b7da1b0cfb6f
Aster Fialla <asterfialla@gmail.com>
2026-02-13 18:18:41 -0500

    refactored bot.js and removed unused methods

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ import { REST } from "@discordjs/rest";
 import { WebSocketManager } from "@discordjs/ws";
 import { db } from './sequelize.js';
 import { webhookHelper } from "./helpers/webhookHelper.js";
-import { proxyHelper } from "./helpers/proxyHelper.js";
 import { messageHelper } from "./helpers/messageHelper.js";
 import { memberHelper } from "./helpers/memberHelper.js";
 
@@ -32,28 +31,28 @@ let plural_flux_name = "";
 let plural_flux_discriminator = "";
 
 client.on(GatewayDispatchEvents.MessageCreate, async ({ api, data }) => {
-    if (!data.content.startsWith(messageHelper.prefix)) {
+    if (data.webhook_id) {
         return;
     }
     else if (data.author.username === plural_flux_name && data.author.discriminator === plural_flux_discriminator) {
         return;
     }
-    else if (data.webhook_id) {
-        return;
+    else if (!data.content.startsWith(messageHelper.prefix)) {
+        const proxyMatch = await messageHelper.parse_proxy_tags(data.author.id, data.content);
+        if (!proxyMatch.proxy) {
+            return;
+        }
+        const member = await memberHelper.get_member_by_proxy(data.author.id, proxyMatch.proxy);
+        await webhookHelper.replace_message(api, data, proxyMatch.message, member);
     }
-    const command_name = data.content.slice(messageHelper.prefix.length).split(" ")[0];
-    const args = messageHelper.parse_message_args(data.content, command_name);
+    else {
+        const command_name = data.content.slice(messageHelper.prefix.length).split(" ")[0];
+        const args = messageHelper.parse_command_args(data.content, command_name);
 
-    if (command_name === "echo") {
-        await echo(api, data, command_name);
-    }
-    else if (command_name === "webhook") {
-        await webhookHelper.get_or_create_webhook(api, data);
-    }
-    else if (command_name === "member") {
-        const reply = await memberHelper.parse_member_command(data.author.id, args);
-        await api.channels.createMessage(data.channel_id, {content: reply});
-
+        if (command_name === "member" || command_name === "m") {
+            const reply = await memberHelper.parse_member_command(data.author.id, args);
+            await api.channels.createMessage(data.channel_id, {content: reply});
+        }
     }
 });
 
@@ -65,7 +64,3 @@ client.on(GatewayDispatchEvents.Ready, async ({data}) => {
 });
 
 gateway.connect();
-
-async function echo(api, data, command_name) {
-    await proxyHelper.replaceMessage(api, data, command_name);
-}
