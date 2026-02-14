d2007f5274126dc0cad896e69053cf62ec3a5cdf
Aster Fialla <asterfialla@gmail.com>
2026-02-14 16:21:46 -0500

    edited import docstrings and logic

--- import.js
+++ import.js
@@ -1,4 +1,3 @@
-import {db} from './sequelize.js'
 import {enums} from "./enums.js";
 import {memberHelper} from "./helpers/memberHelper.js";
 
@@ -8,18 +7,19 @@ const ih = {};
  * Tries to import from Pluralkit.
  *
  * @param {string} authorId - The author of the message
- * @param {string[]} args - The message arguments
- * @returns {Promise<string>} A successful addition.
+ * @param {string} attachment - The attached JSON url.
+ * @returns {string} A successful addition.
  * @throws {Error}  When the member exists, or creating a member doesn't work.
  */
 ih.pluralKitImport = function (authorId, attachment) {
     try {
         const pkData = JSON.parse(attachment);
         const pkMembers = pkData.members;
-        pkMembers.forEach((pkMember) => {
+        pkMembers.forEach(async(pkMember) => {
             const proxy = `${pkMember.proxy_tags.prefix}text${pkMember.proxy_tags.suffix}`;
-            memberHelper.addFullMember(authorId, pkMember.name, pkMember.display_name, proxy, avatar_url);
+            await memberHelper.addFullMember(authorId, pkMember.name, pkMember.display_name, proxy, avatar_url);
         })
+        return "All members imported.";
     }
     catch {
         throw new Error(enums.err.NOT_JSON);
