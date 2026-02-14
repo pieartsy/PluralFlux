b3f565bd83aef55d9e944ce29c8bb6d9551345e5
Aster Fialla <asterfialla@gmail.com>
2026-02-14 11:37:24 -0500

    moving commands to new file

---
+++ commands.js
@@ -0,0 +1,37 @@
+import {messageHelper} from "./helpers/messageHelper.js";
+import {enums} from "./enums.js";
+import {memberHelper} from "./helpers/memberHelper.js";
+import {EmbedBuilder} from "@fluxerjs/core";
+
+const cmds = new Map();
+
+cmds.set('member', {
+    alias: 'm',
+    description: enums.help.SHORT_DESC_MEMBER,
+    async execute(message, client, args) {
+        const reply = await memberHelper.parseMemberCommand(message.author.id, args, message.attachments[0] ?? null);
+        return await message.reply(reply);
+    }
+})
+
+cmds.set('help', {
+    description: enums.help.SHORT_DESC_HELP,
+    async execute(message) {
+        const fields = [...cmds.entries()].map(([name, cmd]) => ({
+            name: `${messageHelper.prefix}${name}`,
+            value: cmd.description,
+            inline: true,
+        }));
+
+        const embed = new EmbedBuilder()
+            .setTitle('Commands')
+            .setDescription(enums.help.PLURALFLUX)
+            .addFields(...fields)
+            .setFooter({ text: `Prefix: ${messageHelper.prefix}` })
+            .setTimestamp();
+
+        await message.reply({ embeds: [embed.toJSON()] });
+    },
+})
+
+export const commands = cmds;
