63594c9819a717d49514f0aca9f1e24d8c8c8bba
Aster Fialla <asterfialla@gmail.com>
2026-02-15 01:47:36 -0500

    edited documentation and added README

---
+++ README.md
@@ -0,0 +1,20 @@
+# PluralFlux
+PluralFlux is a proxybot akin to PluralKit and Tupperbot, but for [Fluxer](https://fluxer.app/). It is written with the [Fluxer.js](https://fluxerjs.blstmo.com/) library.
+
+## Commands
+All commands are prefixed by `pf;`. Currently only a few are implemented.
+
+- `pf;help` - Sends the current list of commands.
+
+- `pf;import` - Imports from PluralKit using the JSON file provided by their export command. Importing from other proxy bots is *TBD*. `pf;import` and attach your JSON file to the message.
+
+- `pf;member` - Accesses the sub-commands related to editing proxy members. The available subcommands are:
+  - `add` - Creates a new member to proxy with, for example: `pf;member jane`. The member name should ideally be short so you can write other commands with it easily.
+You can optionally add a display name after the member name, for example: `pf;member new jane "Jane Doe | ze/hir"`. If it has spaces, put it in __double quotes__. The length limit is 32 characters.
+  - `remove` - Removes a member based on their name, for example: `pf;member remove jane`.
+  - `name` - Updates the name for a specific member based on their current name, for ex: `pf;member john name jane`. The member name should ideally be short so you can write other commands with it easily.
+  - `displayname` - Updates the display name for a specific member based on their name, for example: `pf;member jane "Jane Doe | ze/hir"`.This can be up to 32 characters long. If it has spaces, put it in __double quotes__.
+  - `propic` - Updates the profile picture for the member. Must be in JPG, PNG, or WEBP format and less than 10MB. The two options are:
+    1. Pass in a direct remote image URL, for example: `pf;member jane propic <https://cdn.pixabay.com/photo/2020/05/02/02/54/animal-5119676_1280.jpg>`. You can upload images on sites like <https://imgbb.com/>.
+    2. Upload an attachment directly.
+    **NOTE:** Fluxer does not save your attachments forever, so option #1 is recommended.
--- src/enums.js
+++ src/enums.js
@@ -10,7 +10,7 @@ helperEnums.err = {
     DISPLAY_NAME_TOO_LONG: "The display name is too long. Please limit it to 32 characters or less.",
     PROXY_EXISTS: "A duplicate proxy already exists for one of your members. Please pick a new one, or change the old one first.",
     NO_SUCH_COMMAND: "No such command exists.",
-    PROPIC_FAILS_REQUIREMENTS: "Profile picture must be in JPG, PNG, or WEBP format.",
+    PROPIC_FAILS_REQUIREMENTS: "Profile picture must be in JPG, PNG, or WEBP format and less than 10MB.",
     PROPIC_CANNOT_LOAD: "Profile picture could not be loaded from URL.",
     NO_WEBHOOKS_ALLOWED: "Channel does not support webhooks.",
     NOT_IN_SERVER: "You can only proxy in a server.",
@@ -30,10 +30,10 @@ helperEnums.help = {
     MEMBER: "Accesses the sub-commands related to editing proxy members. The available subcommands are `list`, `add`, `remove`, `displayname`, `proxy`, and `propic`. Add ` --help` to the end of a subcommand to find out more about it, or just send it without arguments.",
     ADD: "Creates a new member to proxy with, for example: `pf;member jane`. The member name should ideally be short so you can write other commands with it easily. \n\nYou can optionally add a display name after the member name, for example: `pf;member new jane \"Jane Doe | ze/hir\"`. If it has spaces, put it in __double quotes__. The length limit is 32 characters.",
     REMOVE: "Removes a member based on their name, for example: `pf;member remove jane`.",
-    NAME: "Updates the name for a specific member based on their current name, for ex: `pf;member jane name jane`. The member name should ideally be short so you can write other commands with it easily.",
+    NAME: "Updates the name for a specific member based on their current name, for ex: `pf;member john name jane`. The member name should ideally be short so you can write other commands with it easily.",
     DISPLAY_NAME: "Updates the display name for a specific member based on their name, for example: `pf;member jane \"Jane Doe | ze/hir\"`.This can be up to 32 characters long. If it has spaces, put it in __double quotes__.",
     PROXY: "Updates the proxy tag for a specific member based on their name. The proxy must be formatted with the tags surrounding the word 'text', for example: `pf;member jane proxy Jane:text` or `pf;member amal proxy [text]` This is so the bot can detect what the proxy tags are.",
-    PROPIC: "Updates the profile picture for the member. Must be in JPG, PNG, or WEBP format. The two options are:\n1. Pass in a direct remote image URL, for example: `pf;member jane propic <https://cdn.pixabay.com/photo/2020/05/02/02/54/animal-5119676_1280.jpg>`. You can upload images on sites like <https://imgbb.com/>.\n2. Upload an attachment directly.\n\n**NOTE:** Fluxer does not save your attachments forever, so option #1 is recommended.",
+    PROPIC: "Updates the profile picture for the member. Must be in JPG, PNG, or WEBP format and less than 10MB. The two options are:\n1. Pass in a direct remote image URL, for example: `pf;member jane propic <https://cdn.pixabay.com/photo/2020/05/02/02/54/animal-5119676_1280.jpg>`. You can upload images on sites like <https://imgbb.com/>.\n2. Upload an attachment directly.\n\n**NOTE:** Fluxer does not save your attachments forever, so option #1 is recommended.",
     IMPORT: "Imports from PluralKit using the JSON file provided by their export command. Importing from other proxy bots is TBD. `pf;import` and attach your JSON file to the message."
 }
 
