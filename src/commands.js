9a2299c2dc8689bbf99328fa52b4f0b9edc1514a
Aster Fialla <asterfialla@gmail.com>
2026-02-14 22:11:28 -0500

    adjusting attachment arguments to work

--- src/commands.js
+++ src/commands.js
@@ -9,7 +9,9 @@ cmds.set('member', {
     description: enums.help.SHORT_DESC_MEMBER,
     async execute(message, client, args) {
         const authorFull = `${message.author.username}#${message.author.discriminator}`
-        const reply = await memberHelper.parseMemberCommand(message.author.id, authorFull, args, message.attachments[0] = null).catch(e =>{throw e});
+        const attachmentUrl = message.attachments.size > 0 ? message.attachments.first().url : null;
+        const attachmentExpires = message.attachments.size > 0 ? message.attachments.first().expires_at : null;
+        const reply = await memberHelper.parseMemberCommand(message.author.id, authorFull, args, attachmentUrl, attachmentExpires).catch(e =>{throw e});
         if (typeof reply === 'string') {
             return await message.reply(reply);
         }
