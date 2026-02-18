a4804c2ea7cf1e7f86ed2668815b42191cc61e07
Aster Fialla <asterfialla@gmail.com>
2026-02-17 20:48:44 -0500

    added babel to convert es modules to cjs

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-/** @type {import('jest').Config} */
-const config = {
+// jest.config.js
+module.exports = {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: "coverage",
     setupFiles: ["<rootDir>/jest.setup.js"], // path to a setup module to configure the testing environment before each test
-    transform: {},
     verbose: true,
+    transform: {
+        "^.+\\.[t|j]sx?$":  require.resolve('babel-jest')
+    },
 };
-
-export default config;
