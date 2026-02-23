813e893afbe2c52c9669a0800efee2e929d5c512
Aster Fialla <asterfialla@gmail.com>
2026-02-23 12:30:31 -0500

    more accurate framework for utils tests

--- tests/helpers/utils.test.js
+++ tests/helpers/utils.test.js
@@ -1,16 +1,5 @@
-jest.mock('../../src/helpers/messageHelper.js')
-
-const {messageHelper} = require("../../src/helpers/messageHelper.js");
-
-jest.mock('../../src/helpers/messageHelper.js', () => {
-    return {messageHelper: {
-            parseProxyTags: jest.fn(),
-            returnBuffer: jest.fn(),
-            returnBufferFromText: jest.fn(),
-        }}
-})
 const {enums} = require("../../src/enums");
-const {webhookHelper} = require("../../src/helpers/utils.js");
+const {utils} = require("../../src/helpers/utils.js");
 
 
 describe('utils', () => {
