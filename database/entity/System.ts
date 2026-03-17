ff4dba3dfd4b57f6186c3aed8286a5068460f4ea
Aster Fialla <asterfialla@gmail.com>
2026-03-17 09:17:15 -0400

    added system model and migration

---
+++ database/entity/System.ts
@@ -0,0 +1,47 @@
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Unique} from "typeorm"
+
+@Entity({name: "System", synchronize: true})
+@Unique("UQ_System_userid", ['userid'])
+@Unique("UQ_System_shortid", ['shortid'])
+export class System {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({
+        nullable: true,
+    })
+    shortid: string
+
+    @Column()
+    userid: string
+
+    @Column({
+        length: 100,
+        nullable: true
+    })
+    name: string
+
+    @Column({
+        type: "varchar",
+        nullable: true,
+        length: 100
+    })
+    fronter: string
+
+    @Column({
+        nullable: true,
+    })
+    grouptag: string
+
+    @Column({
+        nullable: true,
+    })
+    autoproxy: string
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt: Date
+
+    @UpdateDateColumn({ type: 'timestamptz' })
+    updatedAt: Date
+}
